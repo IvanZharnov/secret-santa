721078c94dfb2a1ba264f4833b29ea3db863c7d6
Ivan Zharnov <ivan.zharnov@gmail.com>
2018-12-07 20:32:40 +0300

    Add title and dropdown

--- App.js
+++ App.js
@@ -1,21 +1,80 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Font } from 'expo';
+import {
+  Text,
+  View,
+  ImageBackground,
+  Picker
+} from 'react-native';
+import { Dropdown } from 'react-native-material-dropdown';
+import {
+  Background,
+  Wrapper,
+  DropdownWrapper,
+  Title
+} from './App.styles';
 
 export default class App extends React.Component {
+
+  state = {
+     fontLoaded: false,
+   };
+
+  async componentWillMount() {
+    await Font.loadAsync({
+      Christmas: require('./assets/fonts/CartoonChristmas.ttf')
+    });
+    this.setState({ fontLoaded: true });
+  };
+
+  componentDidMount() {
+
+  };
+
+
   render() {
+    const data = [{
+      value: 'Полина',
+    }, {
+      value: 'Ваня',
+    }, {
+      value: 'Димас',
+    }, {
+      value: 'Некит',
+    }, {
+      value: 'Галя',
+    }, {
+      value: 'Денис',
+    }];
+
+    if (!this.state.fontLoaded) {
+      return <Expo.AppLoading />;
+    }
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+      <View>
+        <Background source={require('./bckgrnd.jpg')}>
+          <Wrapper>
+            <Title>
+              Secret Santa
+            </Title>
+            <DropdownWrapper>
+              <Dropdown
+                label = 'Choose Santa'
+                data = {data}
+                baseColor = '#d70000'
+                dropdownOffset = {{ top: 20, left: 0 }}
+                rippleInsets = {{ top: 0, bottom: -20 }}
+                itemColor = '#880000'
+                selectedItemColor = '#d70000'
+                textColor = '#d70000'
+                pickerStyle = {{borderRadius: 20, backgroundColor: '#d3fece'}}
+                itemCount = {6}
+                fontSize = {15}
+              />
+            </DropdownWrapper>
+          </Wrapper>
+        </Background>
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ App.styles.js
@@ -0,0 +1,27 @@
+import styled from 'styled-components/native';
+
+export const Background = styled.ImageBackground`
+  width: 100%;
+  height: 100%;
+`;
+
+export const Wrapper = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Title = styled.Text`
+  font-size: 58;
+  color: #b0ffa6;
+  font-family: 'Christmas';
+  margin-bottom: 30px;
+`;
+
+export const DropdownWrapper = styled.View`
+  backgroundColor: #b0ffa6;
+  width: 190px;
+  height: 55px;
+  borderRadius: 25px;
+  padding: 0 15px;
+`;
